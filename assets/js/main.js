f50b978cebabbce977f297b27aef9bc3a798d436
androstb <androstb4@gmail.com>
2026-06-28 20:25:35 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,31 +102,7 @@
   }
   window.addEventListener('load', aosInit);
 
-  /**
-   * Countdown timer
-   */
-  function updateCountDown(countDownItem) {
-    const timeleft = new Date(countDownItem.getAttribute('data-count')).getTime() - new Date().getTime();
-
-    const days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-    countDownItem.querySelector('.count-days').innerHTML = days;
-    countDownItem.querySelector('.count-hours').innerHTML = hours;
-    countDownItem.querySelector('.count-minutes').innerHTML = minutes;
-    countDownItem.querySelector('.count-seconds').innerHTML = seconds;
-
-  }
-
-  document.querySelectorAll('.countdown').forEach(function(countDownItem) {
-    updateCountDown(countDownItem);
-    setInterval(function() {
-      updateCountDown(countDownItem);
-    }, 1000);
-  });
-
+ 
   /**
    * Countdown timer
    */
@@ -188,6 +164,44 @@
 
     });
   }
+/**
+ * Navbar ScrollSpy
+ */
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll("#navmenu a");
+
+function navmenuScrollspy() {
+
+  let current = "";
+
+  sections.forEach((section) => {
+
+    const sectionTop = section.offsetTop - 120;
+    const sectionHeight = section.offsetHeight;
+
+    if (
+      window.scrollY >= sectionTop &&
+      window.scrollY < sectionTop + sectionHeight
+    ) {
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navLinks.forEach((link) => {
+
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+
+  });
+
+}
+
+window.addEventListener("load", navmenuScrollspy);
+window.addEventListener("scroll", navmenuScrollspy);
 
 })();
 
